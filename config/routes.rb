72b630ab498c00f6c18e1eb217d47d10f5c241fa
Vitaliy <vk.vital@gmail.com>
2020-04-15 17:53:11 +0300

    Add some UI layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'projects/new'
   root 'static_pages#home'
   get  '/home', to: 'static_pages#home'
   get  '/todo', to: 'projects#new'
+  resources :projects 
+
 end
---
+++ test/integration/project_add_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class ProjectAddTest < ActionDispatch::IntegrationTest
+
+  test "invalid TODO name" do
+    get todo_path
+    assert_no_difference 'Project.count' do
+      post projects_path, params: { project: { name:  "" } }
+    end
+    assert_template 'projects/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid TODO name" do
+    get todo_path
+    assert_difference 'Project.count', 1 do
+      post projects_path, params: { project: { name: "FooBar name" } }
+    end
+    follow_redirect!
+    assert_template 'projects/show'
+    assert_not flash.nil?
+  end
+end
