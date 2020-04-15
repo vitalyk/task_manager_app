98161454b0f09ae28107838e32119d7fdba03cf7
Vitaliy <vk.vital@gmail.com>
2020-04-15 17:53:11 +0300

    Add some UI layout

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,30 @@
 class ProjectsController < ApplicationController
-  def new
+
+  def index
+    @projects = Project.all
   end
+  
+  def new
+    @project = Project.new
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:success] = "TODO List is created!"
+      redirect_to @project
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:name)                                   
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'projects/new'
   root 'static_pages#home'
   get  '/home', to: 'static_pages#home'
   get  '/todo', to: 'projects#new'
+  #get 'projects/new'
+  resources :projects 
+
 end
