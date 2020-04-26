7f005ab2b45d372acb2457dfd5ad0276352c72a7
Vitaliy <vk.vital@gmail.com>
2020-04-26 19:03:28 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,21 @@ class UsersController < ApplicationController
     @projects = @user.projects
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the TODO List App Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
   private
 
     # Confirms a logged-in user.
@@ -22,4 +37,9 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get    '/signup',  to: 'users#new'
 
-  resources :users, only: [:show] do
+  resources :users, only: [:new, :create, :show] do
     resources :projects, only: [:new, :create, :edit, :update, :destroy]
   end
 
